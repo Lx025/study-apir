4facf55f50c19f8d3e080b74597bea4a80ae8072
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 20:59:57 -0300

    feat: add products

---
+++ src/main/java/com/github/lx025/study_apir/model/Product.java
@@ -0,0 +1,24 @@
+package com.github.lx025.study_apir.model;
+
+public class Product {
+
+    private long id;
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
