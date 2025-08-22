354c5cd9b293bf75b4affdf93308b4a8c80e49ee
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 22:32:21 -0300

    feat: add info

--- src/main/java/com/github/lx025/study_apir/controller/ControllerPing.java
+++ src/main/java/com/github/lx025/study_apir/controller/ControllerPing.java
@@ -16,4 +16,10 @@ public class ControllerPing {
     public String version() {
         return "versão: " + version;
     }
+
+    @GetMapping("/info")
+    public String info() {
+        return "versão: " + version;
+    }
+
 }
